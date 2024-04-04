2258d556c4fa932730ac3d55364bae5a67613d0d
HengyueL <lianghengyue1993@outlook.com>
2024-04-04 13:56:52 -0500

    Update DIP

--- model/__init__.py
+++ model/__init__.py
@@ -2,7 +2,7 @@ from .skip import skip
 import torch.nn as nn
 
 
-def get_net(input_depth=3, NET_TYPE="skip", pad="reflection", upsample_mode='bilinear', n_channels=3, act_fun='LeakyReLU', skip_n33d=128, skip_n33u=128, skip_n11=4, num_scales=5, downsample_mode='stride'):
+def get_net_dip(input_depth=3, NET_TYPE="skip", pad="reflection", upsample_mode='bilinear', n_channels=3, act_fun='LeakyReLU', skip_n33d=128, skip_n33u=128, skip_n11=4, num_scales=5, downsample_mode='stride'):
     if NET_TYPE == 'skip':
         net = skip(input_depth, n_channels, num_channels_down = [skip_n33d]*num_scales if isinstance(skip_n33d, int) else skip_n33d,
                                             num_channels_up =   [skip_n33u]*num_scales if isinstance(skip_n33u, int) else skip_n33u,
--- playground_dip.py
+++ playground_dip.py
@@ -1,18 +1,25 @@
-import argparse
-import torch, os
-import cv2
+import argparse, math, torch, os, cv2
 from imwatermark import WatermarkEncoder, WatermarkDecoder
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage.metrics import peak_signal_noise_ratio as compare_psnr
 import torchvision.transforms as tf
-from model import get_net
+from model import get_net_dip
+from utils import compute_psnr as compute_psnr_tensor
 # import warnings
 # warnings.filterwarnings("ignore")
 
 MEAN = [0.485, 0.456, 0.406]
 STD = [0.229, 0.224, 0.225]
 dimension = 224
+
+
+def compute_psnr_np(a, b):
+    mse = np.mean((a-b)**2)
+    if mse == 0:
+        return 100
+    else:
+        return -10 * math.log10(mse)
 
 
 def plot_image(image_arr, save_name):
@@ -59,15 +66,8 @@ def main(args):
         "examples", "ori_imgs", "000000000711.png"
     )
     img_orig_bgr = cv2.imread(img_orig_path)  # bgr image, unit8 code
-    # img_orig_rgb = bgr2rgb(img_orig_bgr)
-    # save_name = os.path.join(vis_root_dir, "img_orig.png")
-    # plot_image(img_orig_rgb, save_name)
 
     # === Encode a watermark ===
-    # watermark_str = "0" * 16 + "1" * 16
-    # watermark_gt = np.asarray(
-    #     [int(i) for i in watermark_str]
-    # )
     watermark_gt = np.random.binomial(1, 0.5, 32)
     watermark_str = "".join([str(i) for i in watermark_gt.tolist()])
     watermark = watermark_str.encode('utf-8')
@@ -79,31 +79,33 @@ def main(args):
     # Encode watermark
     img_watermarked_bgr = img_orig_bgr  # packed rivaGan encoder works with bgr images
     for _ in range(1):
+        encoder.loadModel()
         img_watermarked_bgr = encoder.encode(img_watermarked_bgr, "rivaGan")
-    # # Visualize watermarked image
-    # vis_img = bgr2rgb(img_watermarked_bgr)
-    # save_name = os.path.join(vis_root_dir, "image_watermarked.png")
-    # plot_image(vis_img, save_name)
-    # Decode watermark (for verification)
-    psnr_orig_watermarked = compare_psnr(img_orig_bgr.astype(np.int16), img_watermarked_bgr.astype(np.int16), data_range=255)
-    print("PSNR [Orig - Watermarked] - {%.04f}" % psnr_orig_watermarked)
 
+    # Check watermarked image quality (same)
+    psnr_orig_watermarked = compare_psnr(img_orig_bgr.astype(np.int16), img_watermarked_bgr.astype(np.int16), data_range=255)
+    print("PSNR [Orig - Watermarked] - %.04f" % psnr_orig_watermarked)
+    # psnr_orig_watermarked = compute_psnr_np(img_orig_bgr.astype(np.float32)/255., img_watermarked_bgr.astype(np.float32)/255.)
+    # print("PSNR [Orig - Watermarked] - {%.04f}" % psnr_orig_watermarked)
+
+    decoder.loadModel()
     watermark_decoded = decoder.decode(img_watermarked_bgr, "rivaGan")
     watermark_decoded_str = "".join([str(i) for i in watermark_decoded.tolist()])
     print("Watermarked img Decoding: ", watermark_decoded_str)
     bitwise_acc = np.mean(watermark_decoded == watermark_gt)
-    print("  Bitwise Acc. - {:.4f} \% ".format(bitwise_acc * 100))
+    print("  Bitwise Acc. - {:.4f} % ".format(bitwise_acc * 100))
     
     # === Construct DIP ===
-    model = get_net().to(device, dtype=torch.float)
+    model = get_net_dip().to(device, dtype=torch.float)
     img_watermarked_bgr_float = uint8_to_float(img_watermarked_bgr)
     img_watermarked_bgr_tensor = img_np_to_tensor(img_watermarked_bgr_float, device)  # DIP input tensor
     # DIP params
-    show_every = 50
+    show_every = 5
     exp_weight = 0.99  # Exponential smoothing factor
-    total_iters = 1000
-    LR = 0.01
-    reg_noise_std = 1./20.
+    total_iters = 200
+    LR = 0.001
+    # reg_noise_std = 1./20.
+    reg_noise_std = 0
     params = model.parameters()
     optimizer = torch.optim.Adam(params, lr=LR)
     # loss_func = torch.nn.MSELoss()
@@ -111,7 +113,12 @@ def main(args):
     
     # Optimize
     model.train()
-    out_avg = None
+    # Log Result
+    iter_log = [] 
+    bitwise_acc_log = []
+    psnr_log = []
+    best_recon = None
+    best_psnr = -float("inf")
     for num_iter in range(total_iters):
         optimizer.zero_grad()
 
@@ -121,11 +128,6 @@ def main(args):
             net_input = img_watermarked_bgr_tensor
         out = model(net_input)
 
-        # Smoothing
-        if out_avg is None:
-            out_avg = out.detach()
-        else:
-            out_avg = out_avg * exp_weight + out.detach() * (1 - exp_weight)
         total_loss = loss_func(out, img_watermarked_bgr_tensor)
         total_loss.backward()
         optimizer.step()
@@ -133,36 +135,52 @@ def main(args):
         # For monitor purpose
         img_recon = np.transpose(torch.clamp(out.detach().cpu(), 0, 1).numpy()[0, :, :, :], [1, 2, 0])
         img_recon_np_int = float_to_int(img_recon)
-        img_recon_smoothed = np.transpose(torch.clamp(out_avg.detach().cpu(), 0, 1).numpy()[0, :, :, :], [1, 2, 0])
-        img_recon_np_smoothed_int = float_to_int(img_recon_smoothed)
-
-        # psnr_recon_watermarked = compare_psnr(img_watermarked_bgr.astype(np.int16), img_recon_np_int, data_range=255)
-        # psnr_orig_recon = compare_psnr(img_orig_bgr.astype(np.int16), img_recon_np_int, data_range=255)
-        # psnr_recon_watermarked_smoothed = compare_psnr(img_watermarked_bgr.astype(np.int16), img_recon_np_smoothed_int, data_range=255)
-        # print("Iter [%d] - Loss [%.06f]" % (num_iter, total_loss.item()))
-        # print("  <PSNR> --- rec|water %.02f --- rec|orig %.02f --- recon_sm|water %.02f " % (psnr_recon_watermarked, psnr_orig_recon, psnr_recon_watermarked_smoothed))
 
         # Checkpoint Vis
-        # if num_iter % show_every == 0 and num_iter > 0:
         if num_iter % show_every == 0:
             psnr_recon_watermarked = compare_psnr(img_watermarked_bgr.astype(np.int16), img_recon_np_int, data_range=255)
             psnr_orig_recon = compare_psnr(img_orig_bgr.astype(np.int16), img_recon_np_int, data_range=255)
-            psnr_recon_watermarked_smoothed = compare_psnr(img_watermarked_bgr.astype(np.int16), img_recon_np_smoothed_int, data_range=255)
             print("Iter [%d] - Loss [%.06f]" % (num_iter, total_loss.item()))
-            print("  <PSNR> --- rec|water %.02f --- rec|orig %.02f --- recon_sm|water %.02f " % (psnr_recon_watermarked, psnr_orig_recon, psnr_recon_watermarked_smoothed))
+            print("  <PSNR> --- rec|water %.02f --- rec|orig %.02f " % (psnr_recon_watermarked, psnr_orig_recon))
 
             # Decode the recon and compute the bitwise-acc
             img_recon_bgr = img_recon_np_int.astype(np.uint8)
             recon_decoding = decoder.decode(img_recon_bgr, "rivaGan")
             recon_decoding_str = "".join([str(i) for i in recon_decoding.tolist()])
             bitwise_acc = np.mean(recon_decoding == watermark_gt)
-            print("  Recon Bitwise Acc. - {:.4f} \% ".format(bitwise_acc * 100))
-            print("  Recon code: {}".format(recon_decoding_str))
+            print("  Recon Bitwise Acc. - {:.4f} % ".format(bitwise_acc * 100))
+            # print("  Recon code: {}".format(recon_decoding_str))
 
-            img_recon_rgb = bgr2rgb(img_recon_bgr)
-            save_name = os.path.join(vis_root_dir, "img_recon_iter_%d.png" % num_iter)
-            plot_image(img_recon_rgb, save_name)
+            # Log result
+            iter_log.append(num_iter)
+            psnr_log.append(psnr_recon_watermarked)
+            bitwise_acc_log.append(bitwise_acc)
+            if psnr_recon_watermarked > best_psnr and bitwise_acc < 0.75:
+                best_psnr = psnr_recon_watermarked
+                img_recon_rgb = bgr2rgb(img_recon_bgr)
+                # best_recon = img_recon_rgb
+                best_recon = img_recon_bgr
 
+    # Summarize Result
+    fig, ax = plt.subplots(ncols=1, nrows=1)
+    ax.plot(iter_log, psnr_log, label="PSNR")
+    save_name = os.path.join(vis_root_dir, "curve_psnr.png")
+    plt.savefig(save_name)
+    plt.close(fig)
+
+    fig, ax = plt.subplots(ncols=1, nrows=1)
+    ax.plot(iter_log, bitwise_acc_log, label="bitwise-acc")
+    ax.hlines(0.75, np.amin(iter_log), np.amax(iter_log), ls="dashed", color="black", label="detection threshold")
+    ax.hlines(0.25, np.amin(iter_log), np.amax(iter_log), ls="dashed", color="black")
+    save_name = os.path.join(vis_root_dir, "curve_watermark_detection.png")
+    plt.savefig(save_name)
+    plt.close(fig)
+
+    save_name = os.path.join(vis_root_dir, "img_best_recon.png")
+    # plot_image(best_recon, save_name)
+    cv2.imwrite(save_name, best_recon)
+
+    print("Best recon psnr: {:04f}".format(best_psnr))
             
 
 if __name__ == "__main__":
